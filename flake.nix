ccccfe18b72001f3128844432b66665ca44323c7
Norel <norel.evoagile@gmail.com>
2025-03-03 23:01:20 +0900

    Add definition for Julia in flake.nix.

--- flake.nix
+++ flake.nix
@@ -2,13 +2,17 @@
   description = "development project templates";
 
   outputs = { self }: {
-    templates.python = {
+    template.python = {
       path = ./template/python;
       description = "Python project template with poetry2nix";
     };
-    templates.bash = {
+    template.bash = {
       path = ./template/bash;
       description = "Bash project template with bats testing framework.";
+    };
+    template.julia = {
+      path = ./template/julia;
+      description = "Julia project template with testing, documenter and CI.";
     };
   };
 }
