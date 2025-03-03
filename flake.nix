81b79fdb271c4020ffb8112e2e09adfac1f99eba
Norel <norel.evoagile@gmail.com>
2025-03-03 23:08:38 +0900

    Fix name to templates.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "development project templates";
 
   outputs = { self }: {
-    template = {
+    templates = {
       python = {
         path = ./template/python;
         description = "Python project template with poetry2nix";
