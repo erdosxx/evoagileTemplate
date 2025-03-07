8bf82b26356f0dec7824e5a8fae3cfdfedef37cd
Norel <norel.evoagile@gmail.com>
2025-03-07 19:21:30 +0900

    Add uncovered sample code.

--- template/julia/src/sample/sampleMod.jl
+++ template/julia/src/sample/sampleMod.jl
@@ -4,4 +4,8 @@ function addone(a::Int)
   return a + 1
 end
 
+function uncovered_add(a::Int)
+  return a + 2
+end
+
 end
--- template/julia/src/sample2/sampleMod2.jl
+++ template/julia/src/sample2/sampleMod2.jl
@@ -4,4 +4,8 @@ function square(a::Int)
   return a * a
 end
 
+function uncovered_func(a::Int)
+  return a * a + 1
+end
+
 end
