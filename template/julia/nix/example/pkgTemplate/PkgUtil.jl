048e8bbdda6e8fdaa110f4a2aab2104f751d868b
Norel <norel.evoagile@gmail.com>
2025-03-17 10:57:50 +0900

    Fix asset path.

--- template/julia/nix/example/pkgTemplate/PkgUtil.jl
+++ template/julia/nix/example/pkgTemplate/PkgUtil.jl
@@ -66,7 +66,7 @@ function genGithubRepo(userName::String, repoName::String, dir::String)
         ),
         make_jl = joinpath(@__DIR__, "make.jlt"),
         index_md = joinpath(@__DIR__, "index.md"),
-        assets=String["assets/citations.css"],
+        assets=String["citations.css"],
       ),
       PT.Dependabot()
     ])
